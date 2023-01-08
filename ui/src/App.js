d92b97dcba2fe3fffe75be0c644921cb1085987d
Paul <paul@e-swoosh.co.uk>
2023-01-08 22:38:15 +0000

    Potentially unsafe link in older browsers

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,11 @@
 import { Box, Grid, Typography } from '@mui/material';
 import SoftwareList from './Components/SoftwareList/SoftwareList';
+import ReactGA from 'react-ga';
 
 function App() {
+  const TRACKING_ID = "UA-XXXXX-X"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <>
       <Grid container spacing={2} justifyContent="center" sx={{ my: 5 }}>
@@ -14,7 +18,7 @@ function App() {
             <Typography variant="h1" sx={{ fontSize: '1.8em' }} gutterBottom>Software size tool</Typography>
             <Typography variant="subtitle1" sx={{ fontStyle: 'italic' }} gutterBottom>Ever wondered how much storage or how big a download will be? Wonder no more with this list!</Typography>
 
-            <Typography variant="body1" gutterBottom>Select table headings to sort &amp; filter. To submit a new record please <a href="https://github.com/TakesTheBiscuit/software-size-tool/issues/new" target="_blank">use the form on GitHub</a>.</Typography>
+            <Typography variant="body1" gutterBottom>Select table headings to sort &amp; filter. To submit a new record please <a href="https://github.com/TakesTheBiscuit/software-size-tool/issues/new">use the form on GitHub</a>.</Typography>
             <Typography variant="body1" gutterBottom>The sizes below are observed and accurate - they are not estimates.</Typography>
           </Box>
         </Grid>
